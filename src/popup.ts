48b656e0b7c7d27081fc1da7bf8d86c49f9a0417
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-21 13:42:28 +0100

    changed to (as HTMLElement) to remove errors

--- src/popup.ts
+++ src/popup.ts
@@ -1,22 +1,22 @@
 export{};
 
 // klickar enbart på första knappen. Knyt läs-mer-knapp till produkt som ska visas? 
-const readMoreButtons = document.querySelector('.read-more-btn');
+const readMoreButton = document.querySelector('.read-more-btn');
 const popupWrapper = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
 const popup = document.querySelector('.popup');
 
-readMoreButtons?.addEventListener('click', () => {
-	popupWrapper!.style.display = 'block';
+readMoreButton?.addEventListener('click', () => {
+	(popupWrapper as HTMLElement).style.display = 'block';
 })
 
 close?.addEventListener('click', () => {
-	popupWrapper!.style.display = 'none';
+	(popupWrapper as HTMLElement).style.display = 'none';
 });
 
-popupWrapper?.addEventListener('click', (e) => {
+popupWrapper?.addEventListener('click', () => {
 
-	popupWrapper.style.display = 'none';
+	(popupWrapper as HTMLElement).style.display = 'none';
 });
 
 popup?.addEventListener('click', (e) => {
