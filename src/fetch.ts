1c41cb6dd6492d94e4d2dde90fa892afc8ac810f
Linnea Saxvik <linnea.saxvik@hotmail.com>
2022-12-21 18:47:44 +0100

    new way of fetch, not using interface

--- src/fetch.ts
+++ src/fetch.ts
@@ -4,6 +4,8 @@ import { IProducts } from "./interfaces";
 //const test = console.log('test')
 
 //function för att hämta produktinfo från api
+//orginal
+/*
 export const fetchProducts = async () => {
     const res = await fetch('https://bortakvall.se/api/products')
     if (!res.ok) {
@@ -12,15 +14,25 @@ export const fetchProducts = async () => {
 
     return await res.json() as IProducts[]
 
-}
+} */
 
-//en lokal variabel som håller infon vi hämtar från api med produkter
-let products: IProducts[] = []
-
-export const testFetch = async () => {
-    products = await fetchProducts()
-    //console för att testa att det fungerar
-    console.log(products)
+export const fetchProducts = () => {
+    fetch('https://bortakvall.se/api/products')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`${res.status} ${res.statusText}`)
+            }
+            return res.json()
+        })//'data i arrow-funktionen är bara ett namn, den andra i data.data är den viktiga'
+        .then(data => {
+            console.log(data.data)
+            const test = data.data.map((product: { name: string; }) => {
+                return `name" + ${product.name}`
+            })
+            console.log(test)
+        }).catch(err => {
+            console.log(err)
+        })
 }
 
 
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,6 @@
-//export { };
+export { };
 //Interface för product-info
+
 export interface IProducts {
     "status": "success",
     "data": [
@@ -20,3 +21,65 @@ export interface IProducts {
         }
     ]
 }
+
+
+/*
+export interface IProducts {
+
+    "id": number,
+    "name": string,
+    "description": string,
+    "price": number,
+    "on_sale": boolean,
+    "images": {
+        "thumbnail": string,
+        "large": string
+    },
+    "stock_status": string,
+    "stock_quantity": null
+
+
+
+
+} */
+
+/*
+export interface IProducts {
+    status: "success",
+    data: [
+        {
+            id: number,
+            name: string,
+            description: string,
+            price: number,
+            on_sale: boolean,
+            images: {
+                thumbnail: string,
+                large: string
+            },
+            stock_status: string,
+            stock_quantity: null
+
+
+        }
+    ]
+}
+*/
+/*
+export interface IProducts {
+
+
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    on_sale: boolean,
+    images: {
+        thumbnail: string,
+        large: string
+    },
+    stock_status: string,
+    stock_quantity: null
+
+} */
+
