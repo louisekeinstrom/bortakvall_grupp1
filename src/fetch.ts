53fc2697b93a7d8f47bca6eab05f96da11020c9a
Linnea Saxvik <linnea.saxvik@hotmail.com>
2022-12-22 11:55:31 +0100

    rendering in console

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,21 +1,23 @@
 //export { };
 import { IProducts } from "./interfaces";
 
-//const test = console.log('test')
 
 //function för att hämta produktinfo från api
-//orginal
+//orginal, fungerade dåligt med interface
+
 /*
 export const fetchProducts = async () => {
     const res = await fetch('https://bortakvall.se/api/products')
     if (!res.ok) {
         throw new Error(`${res.status} ${res.statusText}`)
     }
-
     return await res.json() as IProducts[]
 
-} */
+}*/
 
+
+//fungerar bra
+/*
 export const fetchProducts = () => {
     fetch('https://bortakvall.se/api/products')
         .then(res => {
@@ -26,6 +28,7 @@ export const fetchProducts = () => {
         })//'data i arrow-funktionen är bara ett namn, den andra i data.data är den viktiga'
         .then(data => {
             console.log(data.data)
+            //nu är en del av render här, ska försöka dela upp det
             const test = data.data.map((product: { name: string; }) => {
                 return `name" + ${product.name}`
             })
@@ -33,7 +36,45 @@ export const fetchProducts = () => {
         }).catch(err => {
             console.log(err)
         })
+} */
+
+let products = {}
+export const fetchProducts = () => {
+    fetch('https://bortakvall.se/api/products')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`${res.status} ${res.statusText}`)
+            }
+            return res.json()
+        })//'data i arrow-funktionen är bara ett namn, den andra i data.data är den viktiga'
+        .then(data => {
+            console.log(data.data)
+
+            products = data.data.map((product: any) => {
+                return `id: ${product.id} 
+                name:${product.name} 
+                description: ${product.description}
+                price:${product.price}
+                on_sale: ${product.on_sale}
+                images:${product.images.thumbnail},
+                ${product.images.large} 
+                stock_status:${product.stock_status}
+                stock_quantity:${product.stock_quantity} `
+            })
+
+            console.log(products)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
+
+
+
+
+
+
+
 
 
 
