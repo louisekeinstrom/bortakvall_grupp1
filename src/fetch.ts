30cf13d1050f70390fbd467a9593f3eb5c998044
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-23 19:36:16 +0100

    succeeded to show popup when clicking on read-more-btn

--- src/fetch.ts
+++ src/fetch.ts
@@ -71,28 +71,28 @@ export const renderProducts = () => {
             return res.json()
         })
         .then(data => {
-            //console.log(data.data)
+            console.log(data.data)
 
             document.querySelector('.rendering')!.innerHTML = data.data.map((product: any) => {
                 return `
                 <div class="product-container m-3 col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center flex-column">
-            <i class="cart-icon-container text-light fa-solid fa-cart-plus"></i>
-            <img
-              src="https://bortakvall.se${product.images.thumbnail}"
-              alt="Produkt från Bortakväll"
-              class="product-img img-fluid mx-1"
-            />
-            <div
-              class="product-name-price-container d-flex justify-content-between my-1"
-            >
-              <span class="candy-name">${product.name}</span>
-              <span class="candy-price-container">
-                <span class="candy-price">${product.price}</span> kr
-              </span>
-            </div>
-            <p class="description-text">description</p>
-            <button class="read-more-btn btn btn-secondary">Läs mer</button>
-          </div>
+                  <i class="cart-icon-container text-light fa-solid fa-cart-plus" data-product-id="${product.id}"></i>
+                  <img
+                    src="https://bortakvall.se${product.images.thumbnail}"
+                    alt="Produkt från Bortakväll"
+                    class="product-img img-fluid mx-1"
+                  />
+                  <div
+                    class="product-name-price-container d-flex justify-content-between my-1"
+                  >
+                    <span class="candy-name">${product.name}</span>
+                    <span class="candy-price-container">
+                      <span class="candy-price">${product.price}</span> kr
+                    </span>
+                  </div>
+                  <p class="description-text">description</p>
+                  <button class="read-more-btn btn btn-secondary" data-product-id="${product.id}">Läs mer</button>
+                </div>
                   
                `
             })
--- src/popup.ts
+++ src/popup.ts
@@ -1,13 +1,27 @@
 export{};
+import "./fetch"
 
 // klickar enbart på första knappen. Knyt läs-mer-knapp till produkt som ska visas? 
-const readMoreButton = document.querySelector('.read-more-btn');
+// const readMoreButton = document.querySelectorAll('.read-more-btn');
 const popupWrapper = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
 const popup = document.querySelector('.popup');
 
-readMoreButton?.addEventListener('click', () => {
-	(popupWrapper as HTMLElement).style.display = 'block';
+document.addEventListener('click', (e) => {
+	
+	if( (e.target as HTMLButtonElement).tagName === "BUTTON" && (e.target as HTMLButtonElement).dataset.productId){
+		// e.stopPropagation();
+		
+		(popupWrapper as HTMLElement).style.display = 'block'
+
+		// const productId = (e.target as HTMLButtonElement).dataset.productId?
+
+		console.log("You clicked 'Read more'")
+
+	}
+
+
+
 })
 
 close?.addEventListener('click', () => {
