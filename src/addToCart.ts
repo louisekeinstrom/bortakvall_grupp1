06fa4ee56eeb3d29bcfb8f26da21507d049a4bfa
Louise Keinström <louise.keinstrom@outlook.com>
2022-12-28 13:44:52 +0100

    added clickevent to "i"

--- src/addToCart.ts
+++ src/addToCart.ts
@@ -1,5 +1,29 @@
 export { }
 import "./interfaces"
-// import "./fetch"
+import "./fetch"
 import "./popup"
+import { IProducts } from "./interfaces";
+import { IOrder } from "./interfaces";
 
+const cartMenu = document.querySelector(".cart-product")
+
+document.addEventListener('click', (e) => {
+	if( (e.target as HTMLButtonElement).tagName === "I" && (e.target as HTMLButtonElement).dataset.productId)
+console.log("du har klickat på I-elementet")
+
+const addedProducts: [] = []
+console.log(product : IProducts)
+console.log(addedProducts)
+cartMenu?.innerHTML += `<div class="cart-product">
+<img
+src="https://bortakvall.se${product.images.thumbnail}"
+alt="Produkt från Bortakväll"
+class="product-img img-fluid mx-1"
+data-product-id="${product.id}">
+<span class="candy-name">${product.name}</span>
+                    <span class="candy-price-container">
+                      <span class="candy-price">${product.price}</span> kr
+                    </span>
+</div> `
+return addedProducts
+});
