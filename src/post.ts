90db626989c5d53c8cbcfcf870f65905cdc3ef83
Linnea Saxvik <linnea.saxvik@hotmail.com>
2023-01-02 15:48:34 +0100

    post done!!

--- src/post.ts
+++ src/post.ts
@@ -22,7 +22,6 @@ export const contact = document.querySelector('.contact')!
 const wrong = document.querySelector('#wrong')!
 
 
-/*
 
 export const postOrder = async (fullOrder: IOrder) => {
     const res = await fetch('https://bortakvall.se/api/orders', {
@@ -35,30 +34,24 @@ export const postOrder = async (fullOrder: IOrder) => {
     })
     const response = await res.json() as IResponse
 
-
     if (!res.ok) {
-
         throw new Error(`${res.status} ${res.statusText}`)
-
-
     }
     if (response.status === 'fail') {
         console.log('error')
-        //const error = await res.json() as IError
 
+        //för att visa felmeddelande
         contact.classList.add('hide');
         wrong.classList.remove('hide');
         console.log('WRONG')
-        //const error = await res.json() as IError
+
         wrong.innerHTML = `<div>
          <h2>Något gick fel! :(</h2>
          <p>${res.status} ${response.message}</p>
          <p>Vänligen kontrollera din order och dina uppgifter</p>
- 
-         <!--click-event som backar till kunduppgiftena-->
-         <button class="back btn btn-secondary">Tillbaka</button>
          </div>`
-
+        /*<!--click-event som backar till kunduppgiftena-->
+                 <button class="back btn btn-secondary">Tillbaka</button> */
     }
     else {
 
@@ -71,61 +64,11 @@ export const postOrder = async (fullOrder: IOrder) => {
           vardagar
         </p>
         </div>`
+        console.log(`total ${fullOrder.order_total}`)
 
     }
 
-}*/
-
-export const postOrder = async (testOrder: IOrder) => {
-    const res = await fetch('https://bortakvall.se/api/orders', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(testOrder),
-
-    })
-    const response = await res.json() as IResponse
-
-
-    if (!res.ok) {
-
-        throw new Error(`${res.status} ${res.statusText}`)
-
-
-    }
-    if (response.status === 'fail') {
-        console.log('error')
-        //const error = await res.json() as IError
-
-        contact.classList.add('hide');
-        wrong.classList.remove('hide');
-        console.log('WRONG')
-        //const error = await res.json() as IError
-        wrong.innerHTML = `<div>
-         <h2>Något gick fel! :(</h2>
-         <p>${res.status} ${response.message}</p>
-         <p>Vänligen kontrollera din order och dina uppgifter</p>
- 
-         <!--click-event som backar till kunduppgiftena-->
-         <button class="back btn btn-secondary">Tillbaka</button>
-         </div>`
-
-    }
-    else {
-
-        contact.classList.add('hide');
-        confirm.classList.remove('hide');
-        confirm.innerHTML = `<div>
-        <h2>Tack ${response.data.customer_first_name}!:)</h2>
-        <p>
-          Din order med ordernr ${response.data.id} har gått igenom. Leverans inom 2
-          vardagar
-        </p>
-        </div>`
-        console.log(`total ${testOrder.order_total}`)
-
-    }
+    localStorage.clear();
 
 }
 
--- src/submitForm.ts
+++ src/submitForm.ts
@@ -1,12 +1,14 @@
 export { }
-import { IOrder, IProductsExt } from "./interfaces"
+import { IOrder } from "./interfaces"
 import { postOrder } from "./post";
+
+
 //DOM ref till formuläret
 export const form = document.querySelector('.customerInfo') as HTMLFormElement;
 
-//console.log(form.children)
 
-//inputfields
+
+//inputfields från kundformuläret
 
 const firstName = document.querySelector('#firstName') as HTMLInputElement
 const lastName = document.querySelector('#lastName') as HTMLInputElement
@@ -17,108 +19,32 @@ const tel = document.querySelector('#tel') as HTMLInputElement
 const email = document.querySelector('#email') as HTMLInputElement
 
 
-//försök med klass andvänds INTE NU
-/*
-class Customer {
-    customer_first_name: string;
-    customer_last_name: string;
-    customer_address: string;
-    customer_postcode: string;
-    customer_city: string;
-    customer_email: string;
-
-    constructor(f: string, l: string, a: string, p: string, c: string, e: string) {
-        this.customer_first_name = f;
-        this.customer_last_name = l;
-        this.customer_address = a;
-        this.customer_postcode = p;
-        this.customer_city = c;
-        this.customer_email = e;
-    }
-
-}*/
 
 
-
-//tom array där kundinfo ska sparas
-//let fullOrder: IOrder["order_items"] = []
-const getOrder = localStorage.getItem('products_in_cart') ?? '[]'
-
-//const localOrder: IOrder = JSON.parse(getOrder)
+//hämtar productinfo från localStorage
 const productsArray = JSON.parse(localStorage.getItem('products_in_cart') ?? '[]');
+//array med bara order_items och inte överflödig info
 const orderItems = productsArray.map((product: { order_items: any; }) => product.order_items);
 
-/*
+//räknar ut order_total
+const totalPrice = orderItems.reduce((sum: number, items: { item_total: number; }) => {
+    return sum + items.item_total
+}, 0)
 
-let fullOrder = JSON.parse(getOrder)//[]
 
 
-let workOrder: IOrder["order_items"] = []
-
-workOrder.push(fullOrder.product_id, fullOrder.qty, fullOrder.item_price, fullOrder.item_total)
-*/
 
 //submit form eventlistener
 
 form?.addEventListener('submit', async (e: Event) => {
     e.preventDefault();
 
-    //console.log(fullOrder)
     console.log(orderItems)
 
+    console.log(totalPrice)
 
 
-    /*const kund: IOrder = {
-        customer_first_name: firstName.value,
-        customer_last_name: lastName.value,
-        customer_address: adress.value,
-        customer_postcode: postcode.value,
-        customer_city: city.value,
-        customer_email: email.value,
-        order_total: 0,
-        order_items: [{
-            product_id: 6545,
-            qty: 3,
-            item_price: 8,
-            item_total: 24
-        }]
-    }*/
-    const kund = {
-        customer_first_name: firstName.value,
-        customer_last_name: lastName.value,
-        customer_address: adress.value,
-        customer_postcode: postcode.value,
-        customer_city: city.value,
-        customer_email: email.value,
-        order_total: 0,
-
-    }
-
-
-    const order: IOrder["order_items"] = [
-        {
-            product_id: 6545,
-            qty: 3,
-            item_price: 8,
-            item_total: 24
-        }
-    ]
-
-    order.push({
-        product_id: 6604,
-        qty: 2,
-        item_price: 4,
-        item_total: 8
-    })
-
-    /*
-        const totalPrice = order.reduce((sum, items) => {
-            return sum + items.item_total
-        }, 0) */
-
-    //console.log(totalPrice)
-
-    /*const testOrder: IOrder = { //IOrder 
+    const fullOrder: IOrder = {
         customer_first_name: firstName.value,
         customer_last_name: lastName.value,
         customer_address: adress.value,
@@ -126,76 +52,15 @@ form?.addEventListener('submit', async (e: Event) => {
         customer_city: city.value,
         customer_email: email.value,
         order_total: totalPrice,
-        order_items: order
-    }*/
-    const testOrder: IOrder = {
-        customer_first_name: firstName.value,
-        customer_last_name: lastName.value,
-        customer_address: adress.value,
-        customer_postcode: postcode.value,
-        customer_city: city.value,
-        customer_email: email.value,
-        order_total: 32,//totalPrice,
-        order_items: orderItems//workOrder//order
+        order_items: orderItems
 
     }
-    /*order.push({
-        product_id: 6604,
-        qty: 2,
-        item_price: 4,
-        item_total: 8
-    })*/
-    //testOrder.order_items.push(order)
-
-    console.log(testOrder)
 
 
-
-    /*
-        const fullOrder: IOrder = {
-            customer_first_name: firstName.value,
-            customer_last_name: lastName.value,
-            customer_address: adress.value,
-            customer_postcode: postcode.value,
-            customer_city: city.value,
-            customer_email: email.value,
-            order_total: 8,//24 
-            order_items: [
-                {
-                    product_id: 6604,
-                    qty: 2,
-                    item_price: 4,
-                    item_total: 8
-                },
-            ]
-        }
-        */
-    /*
-        const totalPrice = order_items.reduce((sum, items) => {
-            return sum + items.item_total * (fullOrder.order_items.length)
-        }, 0)*/
-
-    //console.log(totalPrice)
-
-
-    /*product_id: 6545,
-                    qty: 3,//3
-                    item_price: 8,
-                    item_total: 24*/
-    // product_id: 6604, qty: 2, item_price: 4, item_total: 8
-
-    //console.log(fullOrder)
-    //console.log(kund, order)
-
-
+    console.log(fullOrder)
 
     //await postOrder(fullOrder)
-    await postOrder(testOrder)
-
-    //await postOrder(kund, order)
-
-
-
+    await postOrder(fullOrder)
 
 })
 
